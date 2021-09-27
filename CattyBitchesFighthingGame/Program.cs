3deacbff5cb49315af172d464975f41559b39c34
Tina Li <tina.li@elev.ga.ntig.se>
2021-09-27 13:13:32 +0200

    CattyB

---
+++ CattyBitchesFighthingGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CattyBitchesFighthingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Welcome to THE CATTY BITCHES FiGHTING GAME");
+            Console.WriteLine("This hoe just took your man. Press 1 to fight, or press 2 to fight");
+            Console.ReadLine();
+        }
+    }
+}
